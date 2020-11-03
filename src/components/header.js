19e91b7e39ad82de2db4dd7ecf47529fa47d9ea9
GeronimoJohn <johnageronimo1@gmail.com>
2020-11-03 15:19:24 +1300

    header scss updated

--- src/components/header.js
+++ src/components/header.js
@@ -5,26 +5,46 @@ import headerStyles from "./header.module.scss"
 const Header = () => {
   return (
     <header className={headerStyles.header}>
-      <h1>Site Title</h1>
+      <h1>
+        <Link className={headerStyles.title} to="/">
+          GeroWorks
+        </Link>
+      </h1>
       <nav>
-        <ul>
+        <ul className={headerStyles.navList}>
           <li>
-            <Link className={headerStyles.link} to="/">
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/"
+            >
               Home
             </Link>
           </li>
           <li>
-            <Link className={headerStyles.link} to="/blog">
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/blog"
+            >
               Blog
             </Link>
           </li>
           <li>
-            <Link className={headerStyles.link} to="/about">
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/about"
+            >
               About Me
             </Link>
           </li>
           <li>
-            <Link className={headerStyles.link} to="/contact">
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/contact"
+            >
               Contact Me
             </Link>
           </li>
